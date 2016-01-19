7ae775958022276e42c266b5d67da577b74913ec
Matteo Comisso <teomatteo89@gmail.com>
2016-01-19 23:41:31 +0100

    Initial Commit

---
+++ todoMVVM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  todoMVVM
+//
+//  Created by Matteo Comisso on 19/01/16.
+//  Copyright © 2016 mcomisso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
