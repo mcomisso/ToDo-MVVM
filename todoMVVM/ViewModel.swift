6552c5ee521667fdde581eb51b808c6d64333bc9
Matteo Comisso <matteo.comisso@me.com>
2016-01-21 00:43:24 +0100

    Private functions to wrap direct handling of arrays

--- todoMVVM/ViewModel.swift
+++ todoMVVM/ViewModel.swift
@@ -10,54 +10,106 @@ import Foundation
 import UIKit
 
 class ToDoViewModel: NSObject {
-    var toDoDataSource: Array<ToDo>
-    var toDoCompleted: Array<ToDo>
-
-    let demo1 = ToDo(withContent: "Primo todo")
-    let demo2 = ToDo(withContent: "Secondo")
+    var todoDataSource: Array<ToDo>
+    var todoCompleted: Array<ToDo>
+    
+    let demo1 = ToDo(withContent: "First Todo")
+    let demo2 = ToDo(withContent: "Second Todo")
     
     override init() {
-        self.toDoDataSource = []
-        self.toDoCompleted = []
+        self.todoDataSource = []
+        self.todoCompleted = []
     }
-
-    // Demo data
+    
+    /**
+     Set a bunch of demo data
+     */
     func completeWithDemoData() {
-        self.toDoDataSource.append(self.demo1)
-        self.toDoDataSource.append(self.demo2)
+        self.appendToDataSource(self.demo1)
+        self.appendToDataSource(self.demo2)
     }
-
-    // Create new todos
+    
+    /**
+     Create new todos
+     
+     - parameter content: The content string of newly created Todo
+     */
     func createNewTodo(content: String) {
         let newTodo = ToDo(withContent: content)
-        self.toDoDataSource.append(newTodo)
+        self.appendToDataSource(newTodo)
     }
-
-    // Handle current completion status of todos
+    
+    /**
+     Handle current completion status of todos
+     
+     - parameter todo: The item to handle
+     */
     func addCompletedTodo(todo: ToDo) {
         todo.completed = true
-        self.toDoCompleted.append(todo)
+        self.appedToCompleted(todo)
     }
-
+    
+    /**
+     Removes the Todo item from the completed array
+     
+     - parameter todo: The item to remove
+     */
     func removeCompletedTodo(todo: ToDo) {
-        todo.completed = false
-        self.toDoCompleted.removeAtIndex(self.toDoCompleted.indexOf(todo)!)
+        self.removeFromCompleted(todo)
     }
-
+    
+    /**
+     Removes all completed Todos
+     */
     func clearCompletedTodo() {
-        for todo in self.toDoCompleted {
-            todo.completed = false
+        for todo in self.todoCompleted {
+            guard let index = self.todoDataSource.indexOf(todo) else {
+                continue
+            }
+            self.todoDataSource.removeAtIndex(index)
         }
-
-        self.toDoCompleted.removeAll()
+        
+        self.todoCompleted.removeAll()
     }
-
+    
     func isCompleted(todo: ToDo) -> Bool {
-        return self.toDoCompleted.contains(todo)
+        return self.todoCompleted.contains(todo)
     }
-
-    func handleToDoAtIndexPath(indexPath: NSIndexPath) {
-        let todo = self.toDoDataSource[indexPath.row]
+    
+    func handleTodoAtIndexPath(indexPath: NSIndexPath) {
+        let todo = self.todoDataSource[indexPath.row]
         todo.completed ? self.removeCompletedTodo(todo) : self.addCompletedTodo(todo)
+    }
+    
+    // Deletion
+    func deleteTodoItem(todo: ToDo) {
+        self.removeFromDataSource(todo)
+        self.removeFromCompleted(todo)
+    }
+}
+
+extension ToDoViewModel {
+    
+    private func appedToCompleted(todo: ToDo) {
+        self.todoCompleted.append(todo)
+    }
+    
+    private func appendToDataSource(todo: ToDo) {
+        self.todoDataSource.append(todo)
+    }
+    
+    private func removeFromDataSource(todo: ToDo) {
+        guard let index = self.todoDataSource.indexOf(todo) else {
+            return
+        }
+        self.todoDataSource.removeAtIndex(index)
+    }
+    
+    private func removeFromCompleted(todo: ToDo) {
+        defer { todo.completed = false }
+        guard let index = self.todoCompleted.indexOf(todo) else {
+            return
+        }
+        self.todoCompleted.removeAtIndex(index)
     }
 }
